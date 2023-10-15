4e86b158d6279f026025127e2ae6aec01a9897a6
shristeethapa <37829369+shristeethapa@users.noreply.github.com>
2023-10-16 08:55:50 +1100

    Update App.java
    comments added

--- src/main/java/com/mycompany/healthcaremanagementsystem/App.java
+++ src/main/java/com/mycompany/healthcaremanagementsystem/App.java
@@ -11,6 +11,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Main application class for the Healthcare Management System.
+ */
 public class App extends Application {
 
     private static Stage mainAppStage; 
@@ -23,6 +26,12 @@ public class App extends Application {
     private static final String DBUSER = "root"; // need to change as per your mysql username
     private static final String DBPASSWORD = "pass2580!"; //// need to change as per your mysql password
 
+    /**
+     * Starts the application by initializing the main stage and displaying the login window.
+     *
+     * @param stage The primary stage for the application.
+     * @throws IOException If an I/O exception occurs while loading the login window.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         mainAppStage = stage;
@@ -30,7 +39,12 @@ public class App extends Application {
         changeWindow("loginForUser.fxml");
         mainAppStage.show();
     }
-    
+
+    /**
+     * Changes the current window by loading the FXML file and updating the stage with the new scene.
+     *
+     * @param fxmlFileName The name of the FXML file to load.
+     */
     public static void changeWindow(String fxmlFileName) {
         try {
             FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlFileName));
@@ -69,34 +83,71 @@ public class App extends Application {
         }
     }
 
+    
     public static void main(String[] args) {
         launch();
     }
 
+    /**
+     * Retrieves the MySQL database connection instance.
+     *
+     * @return The MySQL database connection instance.
+     */
     public static MySQLDatabaseConnection getDb() {
         return mysqlDB;
     }
 
+    /**
+     * Retrieves the currently logged-in user.
+     *
+     * @return The currently logged-in user.
+     */
     public static UsersEntity getLoggedInUser() {
         return loggedInUser;
     }
 
+    /**
+     * Sets the currently logged-in user.
+     *
+     * @param loggedInUser The user to set as currently logged-in.
+     */
     public static void setLoggedInUser(UsersEntity loggedInUser) {
         App.loggedInUser = loggedInUser;
     }
+    
 
+    /**
+     * Retrieves the user being searched for.
+     *
+     * @return The user being searched for.
+     */
     public static UsersEntity getSearchedUser() {
         return searchedUser;
     }
 
+    /**
+     * Sets the user being searched for.
+     *
+     * @param searchedUser The user to set as the one being searched for.
+     */
     public static void setSearchedUser(UsersEntity searchedUser) {
         App.searchedUser = searchedUser;
     }
 
+    /**
+     * Retrieves the patient being searched for.
+     *
+     * @return The patient being searched for.
+     */
     public static PatientEntity getSearchedPatient() {
         return searchedPatient;
     }
 
+    /**
+     * Sets the patient being searched for.
+     *
+     * @param searchedPatient The patient to set as the one being searched for.
+     */
     public static void setSearchedPatient(PatientEntity searchedPatient) {
         App.searchedPatient = searchedPatient;
     }
